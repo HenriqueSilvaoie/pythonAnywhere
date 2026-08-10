41b50209eab8c215638a69197dbb2a1053e193d4
HenriqueSilvaoie <henriqueads417@gmail.com>
2026-08-10 19:19:13 +0000

    aula 040

--- hello.py
+++ hello.py
@@ -1,38 +1,40 @@
-# A very simple Flask Hello World app for you to get started with...
-from flask import Flask, request, redirect, abort, make_response
+from datetime import datetime
+from flask import Flask, render_template, request
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+
 app = Flask(__name__)
+
+bootstrap = Bootstrap(app)
+moment = Moment(app)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
+
 @app.route('/')
-def hello_world():
-#    return '<p>Alterações por meio do PythonAnyWhere -> GitHub</p><table><tr><td><b>Professor:</b></td><td>Professor Fabio Teixeira</td></tr><tr><td><b>Prontuário:</b></td><td>PT23820X</td></tr></table>'
-    return '<h1>Hello World!</h1><h2>Disciplina PTBDSWS</h2>'
+def index():
+    return render_template('index.html', current_time=datetime.utcnow())
 
-@app.route('/user/<name>')
-def user(name):
-    return '<h1>Hello, {}!</h1>'.format(name)
 
-@app.route('/contextorequisicao')
-def contextorequisicao():
-    user_agent = request.headers.get('User-Agent')
-    return '<p>Your browser is {}</p>'.format(user_agent)
+@app.route('/user/<name>/<pront>/<inst>')
+def user(name, pront, inst):
+    return render_template('user.html', name=name, pront=pront, inst=inst)
 
-@app.route('/codigostatusdiferente')
-def codigostatusdiferente():
-    return '<p>Bad request</p>', 400
 
-@app.route('/redirecionamento')
-def redirecionamento():
-    return redirect('https://ptb.ifsp.edu.br')
-
-@app.route('/abortar')
-def abortar():
-    return abort(404)
-
-@app.route('/objetoresposta')
-def objeto_resposta():
-    #objeto de resposta
-    response = make_response('<h1>This document carries a cookie!</h1>')
-
-    #gera o cookie
-    response.set_cookie('answer', '42')
-
-    return response
+@app.route('/contextorequisicao/<name>')
+def contexto(name):
+    return render_template(
+        'contexto.html',
+        name=name,
+        user_agent=request.headers.get('User-Agent'),
+        ip=request.remote_addr,
+        host=request.host
+    )
