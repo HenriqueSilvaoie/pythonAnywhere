bbf66f412c599b093b69edc24d8cddb2d770e9ab
HenriqueSilvaoie <henriqueads417@gmail.com>
2026-08-23 22:50:59 +0000

    Aua 050

--- hello.py
+++ hello.py
@@ -1,56 +1,82 @@
 #from datetime import datetime
-from flask import Flask, render_template, request, session, redirect, url_for, flash
+from flask import Flask, render_template, request, session, redirect, url_for
 from flask_bootstrap import Bootstrap
-from flask_moment import Moment
+from flask_moment import Moment, datetime
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, SelectField, PasswordField
 from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'Abidi@1329'
 
-
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
 class NameForm(FlaskForm):
-    name = StringField('What is your name?', validators= [DataRequired()])
+    name = StringField('Informe o seu nome: ', validators= [DataRequired()])
+    sobrenome = StringField('Informe o seu sobrenome: ', validators= [DataRequired()])
+    inst_ens = StringField('Informe a sua instituição de ensino: ', validators= [DataRequired()])
+    disciplina = SelectField('Informe a disciplina: ', choices=[('DSWA5'), ('DSWA4'), ('Gestão de Projetos')])
     submit = SubmitField('Submit')
 
-@app.errorhandler(404)
-def page_not_found(e):
-    return render_template('404.html'), 404
-
-
-@app.errorhandler(500)
-def internal_server_error(e):
-    return render_template('500.html'), 500
-
-
+#Para que os campos dos formulários sejam exibidos ao enviar os dados:
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = NameForm()
+
     if form.validate_on_submit():
         old_name = session.get('name')
-        if old_name is not None and old_name != form.name.data:
-            flash('Looks like you have changed your name!')
         session['name'] = form.name.data
+        session['sobrenome'] = form.sobrenome.data
+        session['disciplina'] = form.disciplina.data
+        session['inst_ens'] = form.inst_ens.data
         return redirect(url_for('index'))
-    return render_template('index.html',form=form, name = session.get('name'))
+    return render_template('index.html',
+                            form=form,
+                            name = session.get('name'),
+                            sobrenome = session.get('sobrenome'),
+                            disciplina = session.get('disciplina'),
+                            inst_ens = session.get('inst_ens'),
+                            current_time=datetime.utcnow()
+                            )
 
-
-@app.route('/user/<name>/<pront>/<inst>')
-def user(name, pront, inst):
-    return render_template('user.html', name=name, pront=pront, inst=inst)
-
-
-@app.route('/contextorequisicao/<name>')
-def contexto(name):
+@app.route('/')
+def contexto():
     return render_template(
-        'contexto.html',
-        name=name,
-        user_agent=request.headers.get('User-Agent'),
+        'index.html',
         ip=request.remote_addr,
         host=request.host
+    )
+
+class LoginFormulario(FlaskForm):
+    user = StringField(
+        '',
+        validators=[DataRequired()],
+        render_kw={"placeholder": "Usuário ou e-mail"}
+    )
+    senha = PasswordField(
+        '',
+        validators=[DataRequired()],
+        render_kw={"placeholder": "Informe a sua senha"}
+    )
+    enviar = SubmitField('Enviar')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginFormulario()
+    if form.validate_on_submit():
+        session['usuario_login'] = form.user.data
+        return redirect(url_for('loginResponse'))
+
+    return render_template('login.html', form=form, current_time=datetime.utcnow())
+
+@app.route('/loginResponse')
+def loginResponse():
+    usuario = session.get('usuario_login', '')
+
+    return render_template(
+        'loginResponse.html',
+        usuario=usuario,
+        current_time=datetime.utcnow()
     )
 
